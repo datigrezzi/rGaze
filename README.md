e2134983547b98657e353a249ef59259eee5b9d9
Iyad Aldaqre <Iyad@illiair.lan>
2017-11-26 21:12:51 +0100

    updated readme and started pupils function; still work in progress

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 R package for gaze data analysis
 
 In this package you can find some fairly simple functions to process raw gaze data and apply a velocity-based filter to identify fixations/saccades.
+
 I use this package for some of my data analysis and in some workshops I teach on eye tracking data analysis.
 
 It is a work in progress, based on scripts I wrote during my studies at LMU.
@@ -10,6 +11,14 @@ It has all the basic functionality:
 - Apply filter to identify and group raw samples into fixations or saccades.
 - Check if gaze data is inside areas of interest (AOIs)
 
-Also included are some useful funcitons for plotting or calculating some statistics that are not available in other packages (e.g. SEM or error lines for line plots).
+Also included are some useful functions for plotting or calculating some statistics (e.g. SEM or error lines for line plots).
+
+The workflow of using the functions is as follows:
+1. read in a single recording raw data and assign it to a variable in R
+2. apply the ivt() function, specifying which variables in the dataset correspond to the timestamp, x coordinates and y coordinates. Other important parameters of this function are the velocity threshold, output (saccades or fixations), the sampling rate of the eye tracker with which the data was recorded, and whether consecutive nearby fixations/saccades should be grouped.
+3. read in an AOIs file; two types of AOIs are supported: circular and rectangular. This file should be organized with one AOI per column, with the header containing AOI names. For circular AOIs the first and second rows should contain the x and y coordinates of the center of each AOI respectively. The third row should contain the radius of AOIs. For Rectangular AOIs, first and second rows should contain the minimum and maximum X coordinates respectively. Third and fourth rows should contain minimum and maximum Y coordinates of each AOI.
+4. apply the aois() function, which returns a vector of the same number of rows of the dataset passed to it with the AOI names that correspond to each row in the data.
+
+Afterwards one can process the data to calculate the various metrics from the processed data.
 
 Please note that these scripts and any eventual package based on them come with no guarantee and you may use them at your own risk.
--- todo/pupils.R
+++ todo/pupils.R
@@ -2,24 +2,41 @@
 # after calculating average baseline and subtracting it from each sample
 # sketch written on 11.2.2015
 # iyad aldaqre
+require(zoo)
+rm(list=ls())
 
-pupils<-function(data,variables=('recordingTimestamp','mediaName','pupilDiameter'),mediaList, mediaIndex, baseline.segmentTime, pupil.segmentTime, interpolate=T,maxgap=5,bin=ceil(sampling.rate), sampling.rate=60,saveOutput=F,outputDir){
-	require(zoo)
-	require(reshape2)
-	# get studio events data and choose mediaIndex of those (with start and end time)
-	pupilMedia<-mediaList[is.element(mediaList$Index,mediaIndex),variables[2]]
-	warning('Analyzing media: ', paste(pupilMedia, collapse=' '))
+pupil <- function(data,variables=c('Timestamp','Stimulus','Pupil'),baselineTime=c(-1500, -500), pupilTime = c(1000, 2000), bin=ceil(sampling.rate), samplingRate=60, interpolate=T, maxgap=5){
+	# making sure data is consecutive (verifying timestamps)
+	if(max(range(diff(data[,variables[1]])<=ceiling(1000/samplingRate))) > 1){
+		warning("Data might not be continuous!")
+	}
 	# interpolate if explicitly asked to
 	if(interpolate==T){
-		data[,variables[3]]<-na.locf(data[,variables[3]],maxgap=maxgap, na.rm=F)
+		data[,variables[3]]<-na.locf(data[,variables[3]], maxgap=maxgap, na.rm=F)
 	}
-	# making sure data is consecutive (verifying timestamps)
-	data$consecutive<-diff(data[,variables[1]])<=ceiling(1000/sampling.rate)
-	# based on trial.index, calculate adjTime starting at trial beginning
-	
-	
+	# get unique stimulus names
+	stimuli <- unique(as.character(data[data[,variables[2]] != "" & !is.na(data[,variables[2]]), variables[2]]))
+	for(i in stimuli){
+		# based on stimulus name, get start and end time
+		startTime <- min(data[data[,variables[2]] == i,variables[1]])
+		endTime <- max(data[data[,variables[2]] == i,variables[1]])
+		# calculate baseline
+		data$Baseline[data[,variables[2]]==i] <- mean(data[data[,variables[1]] >= (startTime + baselineTime[1]) & data[,variables[1]] <= (startTime + baselineTime[2]),variables[3]], na.rm=T)
+		# correct pupil
+		data$CorrectedPupil[data[,variables[2]]==i] <- data[data[,variables[2]]==i,variables[3]]-data$Baseline[data[,variables[2]]==i]
+		# calculate relative timeline
+		data$RelativeTimestamp[data[,variables[2]]==i] <- data[data[,variables[2]]==i,variables[1]]-startTime
+	}
+	return(data[!is.na(data$RelativeTimestamp),])
 }
 
+setwd('~/SRLABS_Drive/projects/pupilMarketing')
+# setwd('C:/Users/Aldaqre/Google Drive/projects/pupilMarketing')
+rawData <- read.table("IPSOS_Kinder-Pack_Data_Export.tsv",sep='\t',header=T,dec=",")
+inData <- rawData[rawData$ParticipantName=="A001",]
+
+pupilData <- pupil(inData, variables <- c('RecordingTimestamp','MediaName','PupilRight'))
+plot(pupilData$RelativeTimestamp, pupilData$CorrectedPupil, type='l')
 
 # add outlier rejection
 # output variables: raw average pupil, absolute difference form baseline, percent difference from baseline, pupil.segment absolute means and relative changes from baseline and definitely sems for each time point
